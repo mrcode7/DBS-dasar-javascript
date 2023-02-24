96271b1ea896ebfbb9f893ff655efd803e00ca8c
mrcode7 <learningmrcode@gmail.com>
2023-02-25 04:32:23 +0700

    dasar variable

---
+++ dasar-javascript/variable.js
@@ -0,0 +1,12 @@
+// Variable var tidak di rekomendasikan lagi karena bisa mengakibatkan error
+var firstName = "Hasbi";
+
+// Variable let
+let lastName;
+lastName = "Alwi";
+
+// Variable const
+const age = 20;
+
+// Menampilkan variable
+console.log(firstName);
