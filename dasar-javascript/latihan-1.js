3b6aa0f477fa8799141180d09ca1a8bb961876bc
mrcode7 <learningmrcode@gmail.com>
2023-02-25 05:13:23 +0700

    Latihan 1 variable dan type data

---
+++ dasar-javascript/latihan-1.js
@@ -0,0 +1,17 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+let firstName = "Hasbi";
+let lastName = "Alwi";
+const age = 23;
+const isMarried = false;
+
+console.log(`Nama saya ${firstName} ${lastName}, Umur saya ${age} Tahun dan saya belum menikah ${isMarried}`);
+
