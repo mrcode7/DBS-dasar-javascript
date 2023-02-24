cd9caf146f73f6a8ee7d765939a380a42f182daa
mrcode7 <learningmrcode@gmail.com>
2023-02-25 04:40:18 +0700

    Type Data

---
+++ dasar-javascript/typeData.js
@@ -0,0 +1,29 @@
+// Undefined
+
+let x ;
+console.log(typeof x);
+
+// Number
+
+let y = 10;
+console.log(typeof y);
+
+// String
+
+let z = "Hello";
+console.log(typeof z);
+
+// Boolean
+
+let a = true;
+console.log(typeof a);
+
+// null
+
+let b = null;
+console.log(typeof b);
+
+// symbol
+
+let c = Symbol();
+console.log(typeof c);
