aadf560caf1469d0cf15ed1d958d760a53f21e2a
mrcode7 <learningmrcode@gmail.com>
2023-02-26 01:29:00 +0700

    Object data

---
+++ struktur-data/object.js
@@ -0,0 +1,9 @@
+// Object 
+
+const user = {
+    firstName: "Hasbi",
+    lastName: "Alwi",
+    age: 20,
+}
+
+console.log(`Halo nama saya ${user.firstName} ${user.lastName}, umur saya ${user.age} tahun.`);
